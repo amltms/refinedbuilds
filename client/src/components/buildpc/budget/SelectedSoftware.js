95f865e482c59c5069641fd3fb3dad75dcbf779d
amltms <amalthomas587@gmail.com>
2021-05-27 22:07:08 +0100

    design3

--- client/src/components/buildpc/budget/SelectedSoftware.js
+++ client/src/components/buildpc/budget/SelectedSoftware.js
@@ -2,7 +2,7 @@ export const SelectedSoftware = ({software}) => {
 
     return (
         <div className='software-item' >
-            <img height="100" width="100" src={`/images/software/${software.useCase}/${software.img}`} />
+            <img src={`/images/software/${software.useCase}/${software.img}`} />
             <p>{software.title}</p>
         </div>
     )
