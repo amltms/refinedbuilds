2a440b52ec7f276cba1b59a396dedb1e5da911f6
amltms <amalthomas587@gmail.com>
2021-05-27 22:00:37 +0100

    design2

--- client/src/components/buildpc/software/SoftwareItem.js
+++ client/src/components/buildpc/software/SoftwareItem.js
@@ -9,7 +9,7 @@ export const SoftwareItem = ({selected, software, onClick}) => {
     return (
         <div className={classActve(software)} onClick={() => onClick(software)}>
 
-            <img height="100" width="100" src={`/images/software/${software.useCase}/${software.img}`} />
+            <img src={`/images/software/${software.useCase}/${software.img}`} />
             <p>{software.title}</p>
         </div>
     )
