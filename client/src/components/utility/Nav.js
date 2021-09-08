bdaf13026c2598577ad73f49b6e89a7d5ddb3e3a
amltms <amalthomas587@gmail.com>
2021-09-08 21:09:54 +0100

    fix:homepage

--- client/src/components/utility/Nav.js
+++ client/src/components/utility/Nav.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
-
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 export const Nav = () => {
-    return (
-        <nav>
-            <NavLink activeClassName="active" exact to="/" >Home</NavLink>
-            <NavLink activeClassName="active" exact to="/components" >Components</NavLink>
-            <NavLink activeClassName="active" exact to="/" >Support</NavLink>
-        </nav>
-    )
-}
+  return (
+    <nav>
+      <NavLink activeClassName="active" exact to="/">
+        Home
+      </NavLink>
+      <NavLink activeClassName="active" exact to="/components">
+        Components
+      </NavLink>
+    </nav>
+  );
+};
